98170e69677aabccdf9f7b722ead0bf4069c4267
MINSU KIM <mskim9097@gmail.com>
2022-12-20 21:44:41 +0900

    20221220 home commit

---
+++ src/com/kh/practice/snack/controller/SnackController.java
@@ -0,0 +1,12 @@
+package com.kh.practice.snack.controller;
+
+import com.kh.practice.snack.model.vo.Snack;
+
+public class SnackController {
+	
+	private Snack s = new Snack();
+	
+	public SnackController() {}
+	
+	public String savaData()
+}
